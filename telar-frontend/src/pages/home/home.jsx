e01572cedbdbbdcdbd6b4cfadbd63346781aeef2
Geronimo Gambotto <gero.gambotto@gmail.com>
2023-04-19 22:39:06 -0300

    productshome

--- telar-frontend/src/pages/home/home.jsx
+++ telar-frontend/src/pages/home/home.jsx
@@ -1,16 +1,29 @@
 import React from "react";
-import {Layout} from "../../components/Layout/Layout.jsx";
-import products from "../../products.json"
-
+import { Layout } from "../../components/Layout/Layout.jsx";
+import { products } from "../../products.json";
 import "./style.scss";
 
 export function Home() {
-
-    return <>
-        <Layout>
-            <section className="home-section container-fluid">
-            </section>
-        </Layout>
+  return (
+    <>
+      <Layout>
+        <section className="home-section container-fluid">
+          {products.map((products) => {
+            return (
+              <div key={products.id} value={products.id}>
+                <div className="product">
+                  <img src={products.thumbnail} />
+                  <div>
+                    {products.title}
+                    {products.description}
+                  </div>
+                  <hr />
+                </div>
+              </div>
+            );
+          })}
+        </section>
+      </Layout>
     </>
-    
+  );
 }
