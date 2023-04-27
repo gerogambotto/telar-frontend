5eacbd70f37c93e705001c17d053ce74440a22e8
Geronimo Gambotto <gero.gambotto@gmail.com>
2023-04-27 20:26:12 -0300

    componetizacion

---
+++ telar-frontend/src/pages/product/product.jsx
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from "react";
+import { Layout } from "../../components/Layout/Layout.jsx";
+import "./styles.scss";
+
+export function Product() {
+  return <Layout></Layout>;
+}
